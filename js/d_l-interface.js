a2c30afc5c20c3b66d17214dd895e32748276092
David Wilson <davidtheadmiral@gmail.com>
2017-06-30 13:21:55 -0700

    resolve issues with dom, inserting into object, callback functions, getting appropriate data from api call.

--- js/d_l-interface.js
+++ js/d_l-interface.js
@@ -1,9 +1,18 @@
-var Doctors = require("./../js/d_l.js").doctorModule;
+var Doctor = require("./../js/d_l.js").doctorModule;
+var doctor = new Doctor();
+
+var showDoctors = function(doctor, status){
+  $("#doctor").append("<li>" + doctor + "<ul><li>" + status + "</li></ul></li>");
+};
 
 $(document).ready(function(){
   $("#issueForm").submit(function(event){
     event.preventDefault();
+      $("#doctor").empty();
     var searchIssue = $("#issue").val();
-    
+    // doctor.getDoctors(searchIssue);
+    $("#output").show();
+    $("#issue").append(searchIssue);
+    doctor.getDoctors(searchIssue, showDoctors);
   });
 });
--- js/d_l.js
+++ js/d_l.js
@@ -1,15 +1,27 @@
-function Doctor(){
+var apiKey = require("./../.env").apiKey;
+Doctor = function(){};
 
-}
-
-exports.getDoctors = function(medicalIssue) {
-  $.get('https://api.betterdoctor.com/2016-03-01/doctors?query='+ medicalIssue+'&location=45.5231%2C-122.6765%2C%205&user_location=45.5231%2C-122.6765&skip=0&limit=20&user_key=' + apiKey)
+Doctor.prototype.getDoctors = function(medicalIssue, showDoctors) {
+  $.get('https://api.betterdoctor.com/2016-03-01/doctors?query='+medicalIssue+'&location=45.5231%2C-122.6765%2C%205&user_location=45.5231%2C-122.6765&skip=0&limit=5&user_key=' + apiKey)
    .then(function(result) {
-      console.log(result);
+     var doctorObject = {};
+     doctorObject.name = new Array();
+     doctorObject.status = new Array();
+     for(var i=0;i<result.data.length;i++){
+       console.log(result.data[i].practices[0].name);
+       doctorObject.name.push(result.data[i].practices[0].name);
+       if (result.data[i].practices[0].accepts_new_patients === true) {
+         doctorObject.status.push("Accepting new patients");
+       }
+       else{
+         doctorObject.status.push("Not accepting any more patients");
+       }
+       showDoctors(doctorObject.name[i], doctorObject.status[i]);
+     }
+     console.log(doctorObject);
     })
    .fail(function(error){
       console.log("fail");
     });
 };
-
 exports.doctorModule = Doctor;
